b2effb123b46a9f2983ed3c9d267961303259c8c
x0r5 <obivan05@gmail.com>
2019-07-18 15:47:43 +0200

    clean up code

--- inc/navbar.php
+++ inc/navbar.php
@@ -7,7 +7,7 @@ if(!defined('__CONFIG__')){
 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="/index.php">Navbar</a>
+    <a class="navbar-brand" href="/index.php">TEST</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -24,9 +24,6 @@ if(!defined('__CONFIG__')){
             </li>
             <li class="nav-item <?php setActiveNav("/register.php"); ?>">
                 <a class="nav-link" href="/register.php">Register </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
             </li>
         </ul>
     </div>
