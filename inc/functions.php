33785749bb1f01b8edaba35795fb96d0ddaddc9d
x0r5 <obivan05@gmail.com>
2019-07-19 10:39:39 +0200

    findUser function

--- inc/functions.php
+++ inc/functions.php
@@ -37,4 +37,19 @@ function isLoggedIn(){
     }
 }
 
+function findUser($con, $email, $return_assoc = false){
+    $email = (string) Filter::String($email);
+
+    $findUser = $con->prepare("SELECT user_id, password FROM  users WHERE email = LOWER(:email) LIMIT 1");
+    $findUser->bindParam(':email', $email, PDO::PARAM_STR);
+    $findUser->execute();
+
+    if($return_assoc){
+        return $findUser->fetch(PDO::FETCH_ASSOC);
+    }
+
+    $user_found = (boolean)$findUser->rowCount();
+    return $user_found;
+}
+
 ?>
