2aab1b8ef8f65a081ede1c5402d492f66894594d
x0r5 <obivan05@gmail.com>
2019-07-30 15:04:33 +0200

    new fields at settings

--- ajax/settings.php
+++ ajax/settings.php
@@ -18,14 +18,34 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     //check against stored data
     $con = DB::getConnection();
-    if($email != $__user->email || $name != $__user->name){
-        $update = $con->prepare("UPDATE users SET email = :email, name = :name WHERE user_id = :userid");
+    if($email != $__user->email){
+        $update = $con->prepare("UPDATE users SET email = :email WHERE user_id = :userid");
         $update->bindParam(':email', $email, PDO::PARAM_STR);
+        $update->bindParam(':userid', $__user->user_id, PDO::PARAM_INT);
+        $update->execute();
+        $return['reply'] = "email updated";
+    }
+    if($name != $__user->name){
+        $update = $con->prepare("UPDATE users SET name = :name WHERE user_id = :userid");
         $update->bindParam(':name', $name, PDO::PARAM_STR);
         $update->bindParam(':userid', $__user->user_id, PDO::PARAM_INT);
         $update->execute();
+        $return['reply'] = "name updated";
     }
-    $return['reply'] = "email or username updated";
+    if($street != $__user->address['street']){
+        $update = $con->prepare("UPDATE addresses SET street = :street WHERE user_id = :userid");
+        $update->bindParam(':street', $street, PDO::PARAM_STR);
+        $update->bindParam(':userid', $__user->user_id, PDO::PARAM_INT);
+        $update->execute();
+        $return['reply'] = "street updated";
+    }
+    if($house != $__user->address['house']){
+        $update = $con->prepare("UPDATE addresses SET house = :house WHERE user_id = :userid");
+        $update->bindParam(':house', $house, PDO::PARAM_STR);
+        $update->bindParam(':userid', $__user->user_id, PDO::PARAM_INT);
+        $update->execute();
+        $return['reply'] = "house updated";
+    }
 
 
     $return['error'] = "error happened";
